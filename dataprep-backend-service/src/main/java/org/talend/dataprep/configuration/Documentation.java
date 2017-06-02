cc4e4686e91cb43999d0a818a2141a13f192db9a
Francois Huaulme <fhuaulme@talend.com>
2017-06-02 11:54:58 +0200

    chore(TDP/3545): Code clean up in OS code (#646)
    * Minor fix in Documentation (don't fail if no service.paths set).

--- dataprep-backend-service/src/main/java/org/talend/dataprep/configuration/Documentation.java
+++ dataprep-backend-service/src/main/java/org/talend/dataprep/configuration/Documentation.java
@@ -1,15 +1,14 @@
-//  ============================================================================
+// ============================================================================
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
 //
-//  Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+// This source code is available under agreement available at
+// https://github.com/Talend/data-prep/blob/master/LICENSE
 //
-//  This source code is available under agreement available at
-//  https://github.com/Talend/data-prep/blob/master/LICENSE
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
 //
-//  You should have received a copy of the agreement
-//  along with this program; if not, write to Talend SA
-//  9 rue Pages 92150 Suresnes, France
-//
-//  ============================================================================
+// ============================================================================
 
 package org.talend.dataprep.configuration;
 
@@ -55,7 +54,7 @@ public class Documentation {
     @Value("${service.documentation.description}")
     private String serviceDescription;
 
-    @Value("#{'${service.paths}'.split(',')}")
+    @Value("#{'${service.paths:}'.split(',')}")
     private String[] servicePaths;
 
     @Controller
