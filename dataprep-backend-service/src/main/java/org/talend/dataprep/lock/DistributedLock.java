3b7a9503e4ab7e0d1e75410f8a15537531731468
Francois Huaulme <fhuaulme@talend.com>
2017-10-23 12:11:00 +0200

    chore(TDP-3429): Remove Hazelcast (#835)
    chore(TDP-3429): Remove Hazelcast
    
    [Hazelcast removal]
    * Remove all hazelcast Spring configurations.
    * Remove all hazelcast configurations in properties.
    * Remove hazelcast from dependencies.
    * Add service-wide watcher to close all opened locks on application shutdown.
    
    [configuration]
    * Add properties in configuration "lock.store" to configure lock implementation.
    
    [clean up]
    * Clean up (unused tryLock() method).

--- dataprep-backend-service/src/main/java/org/talend/dataprep/lock/DistributedLock.java
+++ dataprep-backend-service/src/main/java/org/talend/dataprep/lock/DistributedLock.java
@@ -1,15 +1,14 @@
-//  ============================================================================
+// ============================================================================
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
 //
-//  Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+// This source code is available under agreement available at
+// https://github.com/Talend/data-prep/blob/master/LICENSE
 //
-//  This source code is available under agreement available at
-//  https://github.com/Talend/data-prep/blob/master/LICENSE
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
 //
-//  You should have received a copy of the agreement
-//  along with this program; if not, write to Talend SA
-//  9 rue Pages 92150 Suresnes, France
-//
-//  ============================================================================
+// ============================================================================
 package org.talend.dataprep.lock;
 
 import java.util.concurrent.locks.Lock;
@@ -23,18 +22,7 @@ import java.util.concurrent.locks.Lock;
 public interface DistributedLock {
 
     /**
-     * Acquires the lock.
-     * 
-     * If the lock is not available then the current thread becomes disabled for thread scheduling purposes and lies
-     * dormant until the lock has been acquired.
-     * 
-     * Implementation Considerations
-     * 
-     * A Lock implementation may be able to detect erroneous use of the lock, such as an invocation that would cause
-     * deadlock, and may throw an (unchecked) exception in such circumstances. The circumstances and the exception type
-     * must be documented by that Lock implementation.
-     * 
-     * Specified by: lock() in Lock
+     * @see Lock#lock().
      */
     void lock();
 
@@ -45,17 +33,9 @@ public interface DistributedLock {
 
     /**
      * Getter for key used for the lock.
-     * 
+     *
      * @return the key used for the lock
      */
     String getKey();
-
-    /**
-     *
-     * Acquires the lock only if it is free at the time of invocation.
-     * @return {@code true} if the lock was acquired and {@code false} otherwise.
-     * @see Lock#tryLock()
-     */
-    boolean tryLock();
 
 }
--- dataprep-backend-service/src/main/java/org/talend/dataprep/lock/NoOpLockFactory.java
+++ dataprep-backend-service/src/main/java/org/talend/dataprep/lock/NoOpLockFactory.java
@@ -1,24 +1,22 @@
-//  ============================================================================
+// ============================================================================
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
 //
-//  Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+// This source code is available under agreement available at
+// https://github.com/Talend/data-prep/blob/master/LICENSE
 //
-//  This source code is available under agreement available at
-//  https://github.com/Talend/data-prep/blob/master/LICENSE
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
 //
-//  You should have received a copy of the agreement
-//  along with this program; if not, write to Talend SA
-//  9 rue Pages 92150 Suresnes, France
-//
-//  ============================================================================
+// ============================================================================
 
 package org.talend.dataprep.lock;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
-import org.talend.dataprep.configuration.HazelcastSetup;
 
 @Component
-@ConditionalOnMissingBean(HazelcastSetup.class)
+@ConditionalOnProperty(name = "lock.store", havingValue = "none", matchIfMissing = true)
 public class NoOpLockFactory implements LockFactory {
 
     @Override
@@ -30,7 +28,7 @@ public class NoOpLockFactory implements LockFactory {
 
         private final String id;
 
-        public NoOpDistributedLock(String id) {
+        private NoOpDistributedLock(String id) {
             this.id = id;
         }
 
@@ -49,9 +47,5 @@ public class NoOpLockFactory implements LockFactory {
             return id;
         }
 
-        @Override
-        public boolean tryLock() {
-            return true;
-        }
     }
 }
