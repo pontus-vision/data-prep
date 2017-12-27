85611fa554f565dc3573cf1a7e5e9674972f382f
cgall <32335676+tlnd-cgall@users.noreply.github.com>
2017-12-27 15:10:51 +0100

    feat(TDP-4394): remove negative values (#963)
    * TDP-4394: remove negative values
    
    * TDP-4394: minor fix
    
    * TDP-4394 : fix for empty cells
    
    * TDP-4394 : save
    
    * TDP-4394 : fix function
    *add keep blank lines
    *add keep nonNumerics values
    
    * TDP-4394 : minor fix
    
    * feat(TDP-4394): small refacto

---
+++ dataprep-actions/src/test/java/org/talend/dataprep/transformation/actions/clear/RemoveNegativeValuesTest.java
@@ -0,0 +1,317 @@
+// ============================================================================
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// https://github.com/Talend/data-prep/blob/master/LICENSE
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+// ============================================================================
+
+package org.talend.dataprep.transformation.actions.clear;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.talend.dataprep.api.action.ActionDefinition;
+import org.talend.dataprep.api.dataset.ColumnMetadata;
+import org.talend.dataprep.api.dataset.row.DataSetRow;
+import org.talend.dataprep.api.type.Type;
+import org.talend.dataprep.transformation.actions.AbstractMetadataBaseTest;
+import org.talend.dataprep.transformation.actions.ActionMetadataTestUtils;
+import org.talend.dataprep.transformation.actions.category.ActionCategory;
+import org.talend.dataprep.transformation.api.action.ActionTestWorkbench;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.talend.dataprep.api.dataset.ColumnMetadata.Builder.column;
+import static org.talend.dataprep.transformation.actions.ActionMetadataTestUtils.getColumn;
+import static org.talend.dataprep.transformation.actions.ActionMetadataTestUtils.getRow;
+
+public class RemoveNegativeValuesTest extends AbstractMetadataBaseTest<RemoveNegativeValues> {
+
+    private Map<String, String> parameters;
+
+    public RemoveNegativeValuesTest() {
+        super(new RemoveNegativeValues());
+    }
+
+    @Override
+    protected CreateNewColumnPolicy getCreateNewColumnPolicy() {
+        return CreateNewColumnPolicy.NA;
+    }
+
+    @Before
+    public void init() throws IOException {
+        parameters = ActionMetadataTestUtils
+                .parseParameters(RemoveNegativeValuesTest.class.getResourceAsStream("removeNegativeValuesAction.json"));
+    }
+
+    @Test
+    public void testAdapt() throws Exception {
+        assertThat(action.adapt((ColumnMetadata) null), is(action));
+        ColumnMetadata column = column().name("myColumn").id(0).type(Type.STRING).build();
+        assertThat(action.adapt(column), is(action));
+    }
+
+    @Test
+    public void testName() throws Exception {
+        assertEquals(action.getName(), "remove_negative_values");
+    }
+
+    @Test
+    public void testCategory() throws Exception {
+        assertThat(action.getCategory(Locale.US), is(ActionCategory.NUMBERS.getDisplayName(Locale.US)));
+    }
+
+    @Test
+    public void should_delete() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0000", "David Bowie");
+        values.put("0001", "-12");
+        final DataSetRow row = new DataSetRow(values);
+
+        // when
+        ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+
+        // then
+        assertEquals("David Bowie", row.get("0000"));
+        assertEquals("", row.get("0001"));
+    }
+
+    @Test
+    public void should_delete_percentage() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0000", "David Bowie");
+        values.put("0001", "-12%");
+        final DataSetRow row = new DataSetRow(values);
+
+        // when
+        ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+
+        // then
+        assertEquals("David Bowie", row.get("0000"));
+        assertEquals("", row.get("0001"));
+    }
+
+    @Test
+    public void should_delete_with_empty() {
+        // given
+        final DataSetRow row1 = getRow("-12", "-25");
+        final DataSetRow row2 = getRow("", "");
+        final DataSetRow row3 = getRow("-5", "-15");
+
+        // when
+        ActionTestWorkbench.test(Arrays.asList(row1, row2, row3), actionRegistry, factory.create(action, parameters));
+
+        // then
+        assertEquals("", row1.get("0001"));
+        assertEquals("", row2.get("0001") );
+        assertEquals("", row3.get("0001"));
+    }
+
+    @Test
+    public void should_delete_with_string() {
+        // given
+        final DataSetRow row1 = getRow("-12", "-25");
+        final DataSetRow row2 = getRow("", "PORTUGAL");
+        final DataSetRow row3 = getRow("-5", "-15");
+
+        // when
+        ActionTestWorkbench.test(Arrays.asList(row1, row2, row3), actionRegistry, factory.create(action, parameters));
+
+        // then
+        assertEquals("", row1.get("0001"));
+        assertEquals("PORTUGAL", row2.get("0001") );
+        assertEquals("", row3.get("0001"));
+    }
+
+    @Test
+    public void should_not_delete_percentage() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0000", "David Bowie");
+        values.put("0001", "-12%");
+        final DataSetRow row = new DataSetRow(values);
+
+        // when
+        ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+
+        // then
+        assertEquals("David Bowie", row.get("0000"));
+        assertEquals("", row.get("0001"));
+    }
+
+    @Test
+    public void should_delete_alt_format_1() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0000", "David Bowie");
+        values.put("0001", "-1 200");
+        final DataSetRow row = new DataSetRow(values);
+
+        // when
+        ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+
+        // then
+        assertEquals("David Bowie", row.get("0000"));
+        assertEquals("", row.get("0001"));
+    }
+
+    @Test
+    public void should_delete_even_with_leading_space() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0000", "David Bowie");
+        values.put("0001", " -12"); // notice the space before ' Berlin'
+        final DataSetRow row = new DataSetRow(values);
+
+        // when
+        ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+
+        // then
+        assertEquals("David Bowie", row.get("0000"));
+        assertEquals("", row.get("0001"));
+    }
+
+    @Test
+    public void should_delete_even_with_trailing_space() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0000", "David Bowie");
+        values.put("0001", "-12 "); // notice the space after 'Berlin '
+        final DataSetRow row = new DataSetRow(values);
+
+        // when
+        ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+
+        // then
+        assertEquals("David Bowie", row.get("0000"));
+        assertEquals("", row.get("0001"));
+    }
+
+    @Test
+    public void should_delete_even_with_enclosing_spaces() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0000", "David Bowie");
+        values.put("0001", " -12 "); // notice the spaces enclosing ' Berlin '
+        final DataSetRow row = new DataSetRow(values);
+
+        // when
+        ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+
+        // then
+        assertEquals("David Bowie", row.get("0000"));
+        assertEquals("", row.get("0001"));
+    }
+
+    @Test
+    public void should_not_delete_because_value_not_found() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0000", "David Bowie");
+        final DataSetRow row = new DataSetRow(values);
+
+        // when
+        ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+
+        // then
+        assertEquals("David Bowie", row.get("0000"));
+    }
+
+    @Test
+    public void should_not_delete_because_value_different() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0000", "David Bowie");
+        values.put("0001", "68");
+        final DataSetRow row = new DataSetRow(values);
+
+        // when
+        ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+
+        // then
+        assertEquals("David Bowie", row.get("0000"));
+        assertEquals("68", row.get("0001"));
+    }
+
+    @Test
+    public void should_not_delete_because_value_different_zero() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0000", "David Bowie");
+        values.put("0001", "0");
+        final DataSetRow row = new DataSetRow(values);
+
+        // when
+        ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+
+        // then
+        assertEquals("David Bowie", row.get("0000"));
+        assertEquals("0", row.get("0001"));
+    }
+
+    @Test
+    public void should_not_delete_because_null_value() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0000", "Michael Jordan");
+        values.put("0001", null);
+        final DataSetRow row = new DataSetRow(values);
+
+        // when
+        ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+
+        // then
+        assertEquals("Michael Jordan", row.get("0000"));
+        values.put("0001", null);
+    }
+
+    @Test
+    public void should_not_delete_because_nan() {
+        // given
+        final Map<String, String> values = new HashMap<>();
+        values.put("0000", "Michael Jordan");
+        values.put("0001", "undefined");
+        final DataSetRow row = new DataSetRow(values);
+
+        // when
+        ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+
+        // then
+        assertEquals("Michael Jordan", row.get("0000"));
+        values.put("0001", "undefined");
+    }
+
+    @Test
+    public void should_accept_column() {
+        assertTrue(action.acceptField(getColumn(Type.NUMERIC)));
+        assertTrue(action.acceptField(getColumn(Type.FLOAT)));
+    }
+
+    @Test
+    public void should_not_accept_column() {
+        assertFalse(action.acceptField(getColumn(Type.STRING)));
+        assertFalse(action.acceptField(getColumn(Type.DATE)));
+        assertFalse(action.acceptField(getColumn(Type.BOOLEAN)));
+        assertFalse(action.acceptField(getColumn(Type.ANY)));
+    }
+
+    @Test
+    public void should_have_expected_behavior() {
+        assertEquals(1, action.getBehavior().size());
+        assertTrue(action.getBehavior().contains(ActionDefinition.Behavior.VALUES_COLUMN));
+    }
+
+}
