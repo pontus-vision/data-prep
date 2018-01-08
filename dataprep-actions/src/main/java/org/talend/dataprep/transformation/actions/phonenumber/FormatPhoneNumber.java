74d60b94b9d1d183d6d8982f2f0b94e87a1eb0f2
JM Brun <jbrun@talend.com>
2018-01-08 21:15:19 +0100

    fix(TDP-4943/TDP-3798) : bug when action with toggle column option doesn't apply on cell (#1001)
    * Fix bug when action doesn't apply on cell: set empty string on targeted column.

--- dataprep-actions/src/main/java/org/talend/dataprep/transformation/actions/phonenumber/FormatPhoneNumber.java
+++ dataprep-actions/src/main/java/org/talend/dataprep/transformation/actions/phonenumber/FormatPhoneNumber.java
@@ -12,20 +12,6 @@
 // ============================================================================
 package org.talend.dataprep.transformation.actions.phonenumber;
 
-import static java.util.Collections.singletonList;
-import static org.apache.commons.lang.StringUtils.EMPTY;
-import static org.talend.dataprep.parameters.Parameter.parameter;
-import static org.talend.dataprep.parameters.ParameterType.COLUMN;
-import static org.talend.dataprep.parameters.ParameterType.STRING;
-import static org.talend.dataprep.parameters.SelectParameter.selectParameter;
-import static org.talend.dataprep.transformation.actions.common.OtherColumnParameters.*;
-import static org.talend.dataprep.transformation.api.action.context.ActionContext.ActionStatus.CANCELED;
-import static org.talend.dataprep.transformation.api.action.context.ActionContext.ActionStatus.OK;
-
-import java.util.*;
-
-import javax.annotation.Nonnull;
-
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,27 +28,60 @@ import org.talend.dataprep.transformation.actions.common.OtherColumnParameters;
 import org.talend.dataprep.transformation.api.action.context.ActionContext;
 import org.talend.dataquality.standardization.phone.PhoneNumberHandlerBase;
 
+import javax.annotation.Nonnull;
+import java.util.*;
+
+import static java.util.Collections.singletonList;
+import static org.apache.commons.lang.StringUtils.EMPTY;
+import static org.talend.dataprep.parameters.Parameter.parameter;
+import static org.talend.dataprep.parameters.ParameterType.COLUMN;
+import static org.talend.dataprep.parameters.ParameterType.STRING;
+import static org.talend.dataprep.parameters.SelectParameter.selectParameter;
+import static org.talend.dataprep.transformation.actions.common.OtherColumnParameters.*;
+import static org.talend.dataprep.transformation.api.action.context.ActionContext.ActionStatus.CANCELED;
+import static org.talend.dataprep.transformation.api.action.context.ActionContext.ActionStatus.OK;
+
 /**
  * Format a validated phone number to a specified format.
  */
 @Action(FormatPhoneNumber.ACTION_NAME)
 public class FormatPhoneNumber extends AbstractActionMetadata implements ColumnAction {
-
     /**
      * Action name.
      */
     public static final String ACTION_NAME = "format_phone_number"; //$NON-NLS-1$
 
-    /** a region code parameter */
+    protected static final String NEW_COLUMN_SUFFIX = "_formatted";
+
+    static final String OTHER_REGION_TO_BE_SPECIFIED = "other_region";
+
+    /**
+     * the follow 4 types is provided to user selection on UI
+     */
+    static final String TYPE_INTERNATIONAL = "international"; //$NON-NLS-1$
+
+    static final String TYPE_NATIONAL = "national"; //$NON-NLS-1$
+
+    static final String TYPE_E164 = "E164"; //$NON-NLS-1$
+
+    static final String TYPE_RFC3966 = "RFC3966"; //$NON-NLS-1$
+
+    /**
+     * a region code parameter
+     */
     static final String REGIONS_PARAMETER_CONSTANT_MODE = "region_code"; //$NON-NLS-1$
 
-    /** a manually input parameter of region code */
+    /**
+     * a manually input parameter of region code
+     */
     static final String MANUAL_REGION_PARAMETER_STRING = "manual_region_string"; //$NON-NLS-1$
 
-    /** a parameter of format type */
+    /**
+     * a parameter of format type
+     */
     static final String FORMAT_TYPE_PARAMETER = "format_type"; //$NON-NLS-1$
 
-    private static final String PHONE_NUMBER_HANDLER_KEY = "phone_number_handler_helper";//$NON-NLS-1$
+    private static final String PHONE_NUMBER_HANDLER_KEY = "phone_number_handler_helper"; //$NON-NLS-1$
 
     private static final String US_REGION_CODE = "US";
 
@@ -72,20 +91,7 @@ public class FormatPhoneNumber extends AbstractActionMetadata implements ColumnA
 
     private static final String DE_REGION_CODE = "DE";
 
-    static final String OTHER_REGION_TO_BE_SPECIFIED = "other_region";
-
     private static final Logger LOGGER = LoggerFactory.getLogger(FormatPhoneNumber.class);
-
-    /** the follow 4 types is provided to user selection on UI */
-    static final String TYPE_INTERNATIONAL = "international"; //$NON-NLS-1$
-
-    static final String TYPE_NATIONAL = "national"; //$NON-NLS-1$
-
-    static final String TYPE_E164 = "E164"; //$NON-NLS-1$
-
-    static final String TYPE_RFC3966 = "RFC3966"; //$NON-NLS-1$
-
-    protected static final String NEW_COLUMN_SUFFIX = "_formatted";
 
     private static final boolean CREATE_NEW_COLUMN_DEFAULT = false;
 
@@ -110,6 +116,7 @@ public class FormatPhoneNumber extends AbstractActionMetadata implements ColumnA
         final String columnId = context.getColumnId();
         final String possiblePhoneValue = row.get(columnId);
         if (StringUtils.isEmpty(possiblePhoneValue)) {
+            row.set(ActionsUtils.getTargetColumnId(context), possiblePhoneValue);
             return;
         }
 
@@ -130,16 +137,16 @@ public class FormatPhoneNumber extends AbstractActionMetadata implements ColumnA
             return phone;
         }
         switch (formatType) {
-        case TYPE_INTERNATIONAL:
-            return PhoneNumberHandlerBase.formatInternational(phone, regionParam);
-        case TYPE_NATIONAL:
-            return PhoneNumberHandlerBase.formatNational(phone, regionParam);
-        case TYPE_E164:
-            return PhoneNumberHandlerBase.formatE164(phone, regionParam);
-        case TYPE_RFC3966:
-            return PhoneNumberHandlerBase.formatRFC396(phone, regionParam);
-        default:
-            return phone;
+            case TYPE_INTERNATIONAL:
+                return PhoneNumberHandlerBase.formatInternational(phone, regionParam);
+            case TYPE_NATIONAL:
+                return PhoneNumberHandlerBase.formatNational(phone, regionParam);
+            case TYPE_E164:
+                return PhoneNumberHandlerBase.formatE164(phone, regionParam);
+            case TYPE_RFC3966:
+                return PhoneNumberHandlerBase.formatRFC396(phone, regionParam);
+            default:
+                return phone;
         }
     }
 
@@ -164,9 +171,9 @@ public class FormatPhoneNumber extends AbstractActionMetadata implements ColumnA
                                 .item(DE_REGION_CODE, DE_REGION_CODE) //
                                 .item(OTHER_REGION_TO_BE_SPECIFIED, OTHER_REGION_TO_BE_SPECIFIED,
                                         parameter(locale).setName(MANUAL_REGION_PARAMETER_STRING)
-                                        .setType(STRING)
-                                        .setDefaultValue(EMPTY)
-                                        .build(this)).defaultValue(US_REGION_CODE).build(this)) //
+                                                .setType(STRING)
+                                                .setDefaultValue(EMPTY)
+                                                .build(this)).defaultValue(US_REGION_CODE).build(this)) //
                 .defaultValue(CONSTANT_MODE).build(this));
 
         parameters.add(selectParameter(locale).name(FORMAT_TYPE_PARAMETER) //
@@ -182,21 +189,21 @@ public class FormatPhoneNumber extends AbstractActionMetadata implements ColumnA
         final Map<String, String> parameters = context.getParameters();
         final String regionParam;
         switch (parameters.get(OtherColumnParameters.MODE_PARAMETER)) {
-        case CONSTANT_MODE:
-            if (StringUtils.equals(OTHER_REGION_TO_BE_SPECIFIED, parameters.get(REGIONS_PARAMETER_CONSTANT_MODE))) {
-                regionParam = parameters.get(MANUAL_REGION_PARAMETER_STRING);
-            } else {
-                regionParam = parameters.get(REGIONS_PARAMETER_CONSTANT_MODE);
-            }
-            break;
-        case OTHER_COLUMN_MODE:
-            final ColumnMetadata selectedColumn = context.getRowMetadata()
-                    .getById(parameters.get(OtherColumnParameters.SELECTED_COLUMN_PARAMETER));
-            regionParam = row.get(selectedColumn.getId());
-            break;
-        default:
-            regionParam = Locale.getDefault().getCountry();
-            break;
+            case CONSTANT_MODE:
+                if (StringUtils.equals(OTHER_REGION_TO_BE_SPECIFIED, parameters.get(REGIONS_PARAMETER_CONSTANT_MODE))) {
+                    regionParam = parameters.get(MANUAL_REGION_PARAMETER_STRING);
+                } else {
+                    regionParam = parameters.get(REGIONS_PARAMETER_CONSTANT_MODE);
+                }
+                break;
+            case OTHER_COLUMN_MODE:
+                final ColumnMetadata selectedColumn = context.getRowMetadata()
+                        .getById(parameters.get(OtherColumnParameters.SELECTED_COLUMN_PARAMETER));
+                regionParam = row.get(selectedColumn.getId());
+                break;
+            default:
+                regionParam = Locale.getDefault().getCountry();
+                break;
         }
         return regionParam;
     }
--- dataprep-actions/src/test/java/org/talend/dataprep/transformation/actions/phonenumber/FormatPhoneNumberTest.java
+++ dataprep-actions/src/test/java/org/talend/dataprep/transformation/actions/phonenumber/FormatPhoneNumberTest.java
@@ -12,12 +12,6 @@
 // ============================================================================
 package org.talend.dataprep.transformation.actions.phonenumber;
 
-import static org.junit.Assert.*;
-import static org.talend.dataprep.transformation.actions.ActionMetadataTestUtils.getColumn;
-
-import java.io.IOException;
-import java.util.*;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.talend.dataprep.api.action.ActionDefinition;
@@ -32,6 +26,12 @@ import org.talend.dataprep.transformation.actions.category.ActionCategory;
 import org.talend.dataprep.transformation.actions.common.ActionsUtils;
 import org.talend.dataprep.transformation.actions.common.OtherColumnParameters;
 import org.talend.dataprep.transformation.api.action.ActionTestWorkbench;
+
+import java.io.IOException;
+import java.util.*;
+
+import static org.junit.Assert.*;
+import static org.talend.dataprep.transformation.actions.ActionMetadataTestUtils.getColumn;
 
 public class FormatPhoneNumberTest extends AbstractMetadataBaseTest<FormatPhoneNumber> {
 
@@ -72,6 +72,12 @@ public class FormatPhoneNumberTest extends AbstractMetadataBaseTest<FormatPhoneN
     }
 
     @Test
+    public void should_have_expected_behavior() {
+        assertEquals(1, action.getBehavior().size());
+        assertTrue(action.getBehavior().contains(ActionDefinition.Behavior.VALUES_COLUMN));
+    }
+
+    @Test
     public void testParameters() throws Exception {
         final List<Parameter> parameters = action.getParameters(Locale.US);
         assertEquals(7, parameters.size());
@@ -80,12 +86,13 @@ public class FormatPhoneNumberTest extends AbstractMetadataBaseTest<FormatPhoneN
         final SelectParameter useWithParam = (SelectParameter) parameters.get(5);
         assertEquals("Value", useWithParam.getItems().get(1).getLabel());
 
-        final SelectParameter regionsListParam =(SelectParameter) useWithParam.getItems().get(1).getParameters().get(0);
+        final SelectParameter regionsListParam = (SelectParameter) useWithParam.getItems().get(1).getParameters().get(0);
         assertEquals("American standard", regionsListParam.getItems().get(0).getLabel());
     }
 
     @Test
     public void test_apply_in_newcolumn() {
+        // given
         parameters.put(FormatPhoneNumber.REGIONS_PARAMETER_CONSTANT_MODE, "FR");
         parameters.put(FormatPhoneNumber.FORMAT_TYPE_PARAMETER, FormatPhoneNumber.TYPE_INTERNATIONAL);
         parameters.put(OtherColumnParameters.MODE_PARAMETER, OtherColumnParameters.CONSTANT_MODE);
@@ -99,7 +106,35 @@ public class FormatPhoneNumberTest extends AbstractMetadataBaseTest<FormatPhoneN
         expectedValues.put("0000", "+33656965822");
         expectedValues.put("0001", "+33 6 56 96 58 22");
 
+        //when
         ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+
+        // then
+        assertEquals(expectedValues, row.values());
+        ColumnMetadata expected = ColumnMetadata.Builder.column().id(1).name("0000_formatted").type(Type.STRING).build();
+        ColumnMetadata actual = row.getRowMetadata().getById("0001");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test_apply_in_newcolumn_with_empty_values() {
+        // given
+        parameters.put(FormatPhoneNumber.REGIONS_PARAMETER_CONSTANT_MODE, "FR");
+        parameters.put(OtherColumnParameters.MODE_PARAMETER, OtherColumnParameters.CONSTANT_MODE);
+        parameters.put(ActionsUtils.CREATE_NEW_COLUMN, "true");
+        Map<String, String> values = new HashMap<>();
+        values.put("0000", "");
+
+        DataSetRow row = new DataSetRow(values);
+
+        Map<String, Object> expectedValues = new LinkedHashMap<>();
+        expectedValues.put("0000", "");
+        expectedValues.put("0001", "");
+
+        //when
+        ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+
+        // then
         assertEquals(expectedValues, row.values());
         ColumnMetadata expected = ColumnMetadata.Builder.column().id(1).name("0000_formatted").type(Type.STRING).build();
         ColumnMetadata actual = row.getRowMetadata().getById("0001");
@@ -118,6 +153,24 @@ public class FormatPhoneNumberTest extends AbstractMetadataBaseTest<FormatPhoneN
 
         Map<String, Object> expectedValues = new LinkedHashMap<>();
         expectedValues.put("0000", "+33 6 56 96 58 22");
+
+        ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
+        assertEquals(expectedValues, row.values());
+    }
+
+    @Test
+    public void test_default_phone_format() {
+        Map<String, String> values = new HashMap<>();
+        parameters.put(FormatPhoneNumber.REGIONS_PARAMETER_CONSTANT_MODE, "FR");
+        parameters.put(FormatPhoneNumber.FORMAT_TYPE_PARAMETER, "phone_test");
+        parameters.put(OtherColumnParameters.MODE_PARAMETER, OtherColumnParameters.CONSTANT_MODE);
+
+        values.put("0000", "+33656965822");
+
+        DataSetRow row = new DataSetRow(values);
+
+        Map<String, Object> expectedValues = new LinkedHashMap<>();
+        expectedValues.put("0000", "+33656965822");
 
         ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
         assertEquals(expectedValues, row.values());
@@ -465,11 +518,4 @@ public class FormatPhoneNumberTest extends AbstractMetadataBaseTest<FormatPhoneN
         ActionTestWorkbench.test(row, actionRegistry, factory.create(action, parameters));
         assertEquals(expectedValues, row.values());
     }
-
-    @Test
-    public void should_have_expected_behavior() {
-        assertEquals(1, action.getBehavior().size());
-        assertTrue(action.getBehavior().contains(ActionDefinition.Behavior.VALUES_COLUMN));
-    }
-
 }
