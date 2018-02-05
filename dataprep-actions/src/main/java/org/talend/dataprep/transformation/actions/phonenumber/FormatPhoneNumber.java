a6021bdbdcce4eb83a87bca10c349bf6e1598ec4
Aurélien Leboulanger <35097656+aleboulanger@users.noreply.github.com>
2018-02-05 18:11:25 +0100

    fix(TDP-2193) : fix format phone number (#1082)
    * fix(TDP-2193): fix SuggestionTest
    
    * use column domain instead of column type for comparison
    
    * TDP-2193 : add TU

--- dataprep-actions/src/main/java/org/talend/dataprep/transformation/actions/phonenumber/FormatPhoneNumber.java
+++ dataprep-actions/src/main/java/org/talend/dataprep/transformation/actions/phonenumber/FormatPhoneNumber.java
@@ -12,14 +12,6 @@
 // ============================================================================
 package org.talend.dataprep.transformation.actions.phonenumber;
 
-import java.util.EnumSet;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Stream;
-import javax.annotation.Nonnull;
-
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,6 +29,10 @@ import org.talend.dataprep.transformation.api.action.context.ActionContext;
 import org.talend.dataquality.semantic.classifier.SemanticCategoryEnum;
 import org.talend.dataquality.standardization.phone.PhoneNumberHandlerBase;
 
+import javax.annotation.Nonnull;
+import java.util.*;
+import java.util.stream.Stream;
+
 import static java.util.Collections.singletonList;
 import static org.apache.commons.lang.StringUtils.EMPTY;
 import static org.talend.dataprep.parameters.Parameter.parameter;
@@ -44,17 +40,10 @@ import static org.talend.dataprep.parameters.ParameterType.COLUMN;
 import static org.talend.dataprep.parameters.ParameterType.STRING;
 import static org.talend.dataprep.parameters.SelectParameter.selectParameter;
 import static org.talend.dataprep.transformation.actions.category.ScopeCategory.DATASET;
-import static org.talend.dataprep.transformation.actions.common.OtherColumnParameters.CONSTANT_MODE;
-import static org.talend.dataprep.transformation.actions.common.OtherColumnParameters.MODE_PARAMETER;
-import static org.talend.dataprep.transformation.actions.common.OtherColumnParameters.OTHER_COLUMN_MODE;
-import static org.talend.dataprep.transformation.actions.common.OtherColumnParameters.SELECTED_COLUMN_PARAMETER;
+import static org.talend.dataprep.transformation.actions.common.OtherColumnParameters.*;
 import static org.talend.dataprep.transformation.api.action.context.ActionContext.ActionStatus.CANCELED;
 import static org.talend.dataprep.transformation.api.action.context.ActionContext.ActionStatus.OK;
-import static org.talend.dataquality.semantic.classifier.SemanticCategoryEnum.DE_PHONE;
-import static org.talend.dataquality.semantic.classifier.SemanticCategoryEnum.FR_PHONE;
-import static org.talend.dataquality.semantic.classifier.SemanticCategoryEnum.PHONE;
-import static org.talend.dataquality.semantic.classifier.SemanticCategoryEnum.UK_PHONE;
-import static org.talend.dataquality.semantic.classifier.SemanticCategoryEnum.US_PHONE;
+import static org.talend.dataquality.semantic.classifier.SemanticCategoryEnum.*;
 
 /**
  * Format a validated phone number to a specified format.
@@ -248,9 +237,10 @@ public class FormatPhoneNumber extends AbstractMultiScopeAction {
 
     @Override
     public boolean acceptField(ColumnMetadata column) {
+        final String domain = column.getDomain().toUpperCase();
         return Stream.of(PHONE, US_PHONE, UK_PHONE, DE_PHONE, FR_PHONE) //
-                .map(SemanticCategoryEnum::getDisplayName) //
-                .anyMatch(s -> column.getType().equals(s));
+                .map(SemanticCategoryEnum::name) //
+                .anyMatch(domain::equals);
     }
 
     @Override
