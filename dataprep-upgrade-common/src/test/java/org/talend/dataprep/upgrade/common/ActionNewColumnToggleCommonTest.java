fa2b4c31123735c0a0c5b9fdf7d1c7a901a1ce62
Stéphane Kermabon <skermabon@talend.com>
2018-02-14 18:09:26 +0100

    Fix/fhuaulme/toggle migration fix root step (#1122)
    * fix ActionNewColumnToggleCommonTest TU

--- dataprep-upgrade-common/src/test/java/org/talend/dataprep/upgrade/common/ActionNewColumnToggleCommonTest.java
+++ dataprep-upgrade-common/src/test/java/org/talend/dataprep/upgrade/common/ActionNewColumnToggleCommonTest.java
@@ -2,9 +2,12 @@ package org.talend.dataprep.upgrade.common;
 
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.Test;
+import org.talend.dataprep.api.preparation.Action;
 import org.talend.dataprep.api.preparation.PreparationActions;
 import org.talend.dataprep.api.preparation.Step;
 import org.talend.dataprep.preparation.store.PersistentStep;
@@ -18,8 +21,13 @@ public class ActionNewColumnToggleCommonTest {
         final PreparationRepository repository = mock(PreparationRepository.class);
         final PreparationActions actions = mock(PreparationActions.class);
         final PersistentStep step = mock(PersistentStep.class);
+        final Action action = mock(Action.class);
+        List<Action> actionsList = new ArrayList<>();
+        actionsList.add(action);
 
         when(step.id()).thenReturn("step-1");
+        when(actions.getActions()).thenReturn(actionsList);
+        when(action.getName()).thenReturn("action");
         when(repository.list(eq(PreparationActions.class))).thenReturn(Stream.of(actions));
         // Twice "action-1" to pass root preparation action filter.
         when(actions.id()).thenReturn("actions-1", "actions-1", "actions-2");
@@ -40,8 +48,13 @@ public class ActionNewColumnToggleCommonTest {
         final PreparationRepository repository = mock(PreparationRepository.class);
         final PreparationActions actions = mock(PreparationActions.class);
         final PersistentStep step = mock(PersistentStep.class);
+        final Action action = mock(Action.class);
+        List<Action> actionsList = new ArrayList<>();
+        actionsList.add(action);
 
         when(step.id()).thenReturn("step-1");
+        when(actions.getActions()).thenReturn(actionsList);
+        when(action.getName()).thenReturn("action");
         when(repository.list(eq(PreparationActions.class))).thenReturn(Stream.of(actions));
         // Twice "action-1" to pass root preparation action filter.
         when(actions.id()).thenReturn("actions-1", "actions-1", "actions-1"); // same id
@@ -62,8 +75,13 @@ public class ActionNewColumnToggleCommonTest {
         final PreparationRepository repository = mock(PreparationRepository.class);
         final PreparationActions actions = mock(PreparationActions.class);
         final PersistentStep step = mock(PersistentStep.class);
+        final Action action = mock(Action.class);
+        List<Action> actionsList = new ArrayList<>();
+        actionsList.add(action);
 
         when(step.id()).thenReturn(Step.ROOT_STEP.id()); // Listed step is root step
+        when(actions.getActions()).thenReturn(actionsList);
+        when(action.getName()).thenReturn("action");
         when(repository.list(eq(PreparationActions.class))).thenReturn(Stream.of(actions));
         when(actions.id()).thenReturn("actions-1", "actions-2");
         when(repository.list(eq(PersistentStep.class), any())).thenReturn(Stream.of(step));
