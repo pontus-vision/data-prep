fa27bc871c23def9fd80451f755c46e832ac43fc
elogeart <elogeart@talend.com>
2018-02-21 10:05:10 +0100

    fix(TDP-5253): fix convert date not applied after column duplication (#1137)
    (cherry picked from commit cc2d11b)
    
    (cherry picked from commit cd56dd7)

--- dataprep-upgrade/src/test/java/org/talend/dataprep/upgrade/to_2_3_0_PE/MigrateActionsParametersTest.java
+++ dataprep-upgrade/src/test/java/org/talend/dataprep/upgrade/to_2_3_0_PE/MigrateActionsParametersTest.java
@@ -42,8 +42,9 @@ public class MigrateActionsParametersTest extends Base_2_3_0_PE_Test {
         // then
         Optional<Action> numericOpsAction = preparationRepository //
                 .list(PreparationActions.class) //
+                .filter(pa -> pa.getId().equals("cde7e7f945afe3e7e39cb26b00705c7464ba6d3a"))
                 .flatMap(a -> a.getActions().stream()) //
-                .filter(a -> "numeric_ops".equals(a.getName())).findAny();
+                .filter(a -> "numeric_ops".equals(a.getName())).findFirst();
 
         assertTrue(numericOpsAction.isPresent());
         assertEquals(TRUE.toString(), numericOpsAction.get().getParameters().get(CREATE_NEW_COLUMN));
