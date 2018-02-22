ee4f0a572686743c104ad0a5257b8943f53af710
Aurélien Leboulanger <35097656+aleboulanger@users.noreply.github.com>
2018-02-22 14:12:11 +0100

    Revert "fix(TDP-5256): Migration task for FormatPhoneNumber action (#1144)" (#1145)
    This reverts commit 03ab9f335df04c665317cf1141e16504bb79771f.
    
    (cherry picked from commit 4fc36ac)

--- dataprep-upgrade/src/main/java/org/talend/dataprep/upgrade/to_2_3_0_PE/MigrateActionsParameters.java
+++ dataprep-upgrade/src/main/java/org/talend/dataprep/upgrade/to_2_3_0_PE/MigrateActionsParameters.java
@@ -18,8 +18,6 @@ import org.springframework.stereotype.Component;
 import org.talend.dataprep.preparation.store.PreparationRepository;
 import org.talend.dataprep.upgrade.common.ActionNewColumnToggleCommon;
 
-import static org.talend.dataprep.upgrade.model.UpgradeTask.target.USER;
-
 @Component
 public class MigrateActionsParameters extends BaseUpgradeTaskTo_2_3_0_PE {
 
@@ -29,7 +27,6 @@ public class MigrateActionsParameters extends BaseUpgradeTaskTo_2_3_0_PE {
     @Override
     public void run() {
         ActionNewColumnToggleCommon.upgradeActions(preparationRepository);
-        FormatPhoneNumberAction.upgradeActions(preparationRepository);
     }
 
     @Override
