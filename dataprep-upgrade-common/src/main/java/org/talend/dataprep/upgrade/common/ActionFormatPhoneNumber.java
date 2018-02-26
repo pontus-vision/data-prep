53cbe0e9a682168c7bfe890c05256dc957746c26
Francois Huaulme <fhuaulme@talend.com>
2018-02-26 11:16:23 +0100

    fix: Add Split parameter migration (#1151)
    * Add Split parameter migration
    * Put common code to same place.
    
    (cherry picked from commit b13462e)
    
    (cherry picked from commit 8b47fb4)

--- dataprep-upgrade-common/src/main/java/org/talend/dataprep/upgrade/common/ActionFormatPhoneNumber.java
+++ dataprep-upgrade-common/src/main/java/org/talend/dataprep/upgrade/common/ActionFormatPhoneNumber.java
@@ -1,47 +1,25 @@
 package org.talend.dataprep.upgrade.common;
 
+import static org.talend.dataprep.transformation.actions.phonenumber.FormatPhoneNumber.*;
+import static org.talend.tql.api.TqlBuilder.eq;
+
 import java.util.Arrays;
 import java.util.Map;
 
-import org.slf4j.Logger;
 import org.talend.dataprep.api.preparation.Action;
-import org.talend.dataprep.api.preparation.PreparationActions;
-import org.talend.dataprep.api.preparation.Step;
-import org.talend.dataprep.preparation.store.PersistentStep;
 import org.talend.dataprep.preparation.store.PreparationRepository;
 import org.talend.dataprep.transformation.actions.phonenumber.FormatPhoneNumber;
 
-import static org.slf4j.LoggerFactory.getLogger;
-import static org.talend.dataprep.transformation.actions.phonenumber.FormatPhoneNumber.FORMAT_TYPE_PARAMETER;
-import static org.talend.dataprep.transformation.actions.phonenumber.FormatPhoneNumber.OTHER_REGION_TO_BE_SPECIFIED;
-import static org.talend.dataprep.transformation.actions.phonenumber.FormatPhoneNumber.REGIONS_PARAMETER_CONSTANT_MODE;
-import static org.talend.tql.api.TqlBuilder.eq;
-
 public final class ActionFormatPhoneNumber {
-
-    private static final Logger LOGGER = getLogger(ActionFormatPhoneNumber.class);
 
     private ActionFormatPhoneNumber() {
     }
 
     public static void upgradeActions(PreparationRepository preparationRepository) {
-        preparationRepository.list(PreparationActions.class, eq("actions.action", FormatPhoneNumber.ACTION_NAME)) //
-                .peek(action -> {
-                    final String beforeUpdateId = action.id();
-                    action.getActions().forEach(ActionFormatPhoneNumber::updateAction);
-                    action.setId(null);
-                    final String afterUpdateId = action.id();
-
-                    if (!beforeUpdateId.equals(afterUpdateId)) {
-                        LOGGER.debug("Migration changed action id from '{}' to '{}', updating steps", beforeUpdateId,
-                                afterUpdateId);
-                        preparationRepository.list(PersistentStep.class, eq("contentId", beforeUpdateId)) //
-                                .filter(s -> !Step.ROOT_STEP.id().equals(s.id())) //
-                                .peek(s -> s.setContent(afterUpdateId)) //
-                                .forEach(preparationRepository::add);
-                    }
-                }) //
-                .forEach(preparationRepository::add); //
+        ParameterMigration.upgradeParameters(preparationRepository, //
+                eq("actions.action", FormatPhoneNumber.ACTION_NAME), //
+                ActionFormatPhoneNumber::updateAction //
+        );
     }
 
     private static void updateAction(Action action) {
