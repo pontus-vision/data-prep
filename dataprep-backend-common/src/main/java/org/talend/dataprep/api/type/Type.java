789d3cecc4ab4ab030b3c8fa74053ecb3655f9c0
Fabien Abballe <fabballe@talend.com>
2018-04-12 13:56:52 +0200

    fix(TDP-5447): Remove semantic type doesn't work on "change semantic domain" step (#1268)

--- dataprep-backend-common/src/main/java/org/talend/dataprep/api/type/Type.java
+++ dataprep-backend-common/src/main/java/org/talend/dataprep/api/type/Type.java
@@ -105,7 +105,7 @@ public enum Type implements Serializable {
     /**
      * @return the type label for the UI.
      */
-    private String getLabelKey() {
+    public String getLabelKey() {
         return labelKey;
     }
 
