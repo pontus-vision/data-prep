3ecc582c606bb66dfd2d1f6f0778df5f56462c52
Henri-Mayeul de Benque <hmdebenque@talend.com>
2018-04-13 17:41:17 +0200

    feat(TDP-2592): correction after TU failure

--- dataprep-backend-service/src/main/java/org/talend/dataprep/schema/SchemaDetectionConfiguration.java
+++ dataprep-backend-service/src/main/java/org/talend/dataprep/schema/SchemaDetectionConfiguration.java
@@ -3,13 +3,9 @@ package org.talend.dataprep.schema;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.talend.dataprep.schema.csv.CsvDetector;
-import org.talend.dataprep.schema.csv.CsvFormatFamily;
 import org.talend.dataprep.schema.html.HtmlDetector;
-import org.talend.dataprep.schema.html.HtmlFormatFamily;
 import org.talend.dataprep.schema.xls.XlsDetector;
-import org.talend.dataprep.schema.xls.XlsFormatFamily;
 
 @Configuration
 public class SchemaDetectionConfiguration {
@@ -28,49 +24,15 @@ public class SchemaDetectionConfiguration {
     @Bean
     public DataprepSchema dataprepSchema() {
         DataprepSchema dataprepSchema = new DataprepSchema();
-        dataprepSchema.registerDetector(xlsDetector());
-        dataprepSchema.registerDetector(htmlDetector());
-        dataprepSchema.registerDetector(csvDetector());
-        return dataprepSchema;
-    }
-
-//    @Bean
-//    @Order(value = 1)
-    public XlsDetector xlsDetector() {
         XlsDetector xlsDetector = new XlsDetector();
         xlsDetector.getFormatFamily().getSchemaGuesser().setMaxNumberOfColumns(maxNumberOfColumns);
-        return xlsDetector;
+        dataprepSchema.registerDetector(xlsDetector);
+        dataprepSchema.registerDetector(new HtmlDetector());
+        CsvDetector detector = new CsvDetector();
+        detector.getFormatFamily().setDefaultEscapeChar(defaultEscapeChar);
+        detector.getFormatFamily().setDefaultTextEnclosure(defaultTextEnclosure);
+        dataprepSchema.registerDetector(detector);
+        return dataprepSchema;
     }
-
-//    @Bean(XlsFormatFamily.BEAN_ID)
-    public XlsFormatFamily xlsFormatFamily() {
-        return xlsDetector().getFormatFamily();
-    }
-
-//    @Bean
-//    @Order(value = 2)
-    public HtmlDetector htmlDetector() {
-        return new HtmlDetector();
-    }
-
-//    @Bean(HtmlFormatFamily.BEAN_ID)
-    public HtmlFormatFamily htmlFormatFamily() {
-        return htmlDetector().getFormatFamily();
-    }
-//
-//    @Bean
-//    @Order(value = 3)
-    public CsvDetector csvDetector() {
-        return new CsvDetector();
-    }
-
-//    @Bean(CsvFormatFamily.BEAN_ID)
-    public CsvFormatFamily csvFormatFamily() {
-        CsvFormatFamily csvFormatFamily = csvDetector().getFormatFamily();
-        csvFormatFamily.setDefaultTextEnclosure(defaultTextEnclosure);
-        csvFormatFamily.setDefaultEscapeChar(defaultEscapeChar);
-        return csvFormatFamily;
-    }
-
 
 }
