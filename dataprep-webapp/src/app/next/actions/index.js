f8ca03780d0c514d81a98ed70da95bc58516ff58
Nicolas Comont <ncomont@talend.com>
2018-05-24 15:57:42 +0200

    work on search

--- dataprep-webapp/src/app/next/actions/index.js
+++ dataprep-webapp/src/app/next/actions/index.js
@@ -2,10 +2,12 @@ import { default as redirect } from './redirect';
 import { default as preparation } from './preparation';
 import { default as help } from './help';
 import { default as version } from './version';
+import { default as search } from './search';
 
 export default {
 	redirect,
 	preparation,
 	help,
 	version,
+	search,
 };
--- dataprep-webapp/src/app/next/sagas/search.saga.js
+++ dataprep-webapp/src/app/next/sagas/search.saga.js
@@ -1,16 +1,56 @@
-import { actions } from '@talend/react-cmf';
-import { take, put, select } from 'redux-saga/effects';
+import { all, takeLatest, call, delay } from 'redux-saga/effects';
+import http from '@talend/react-cmf/lib/sagas/http';
+import { SEARCH_FOR } from '../constants';
 
-function* toggle() {
-	const path = ['Typeahead', 'headerbar:search'];
+function getDocumentationSearchParameters(term) {
+	return [
+		http.post,
+		'https://www.talendforge.org/find/api/THC.php',
+		{
+			contentLocale: 'en',
+			filters: [
+				{ key: 'version', values: ['2.1'] },
+				{ key: 'EnrichPlatform', values: ['Talend Data Preparation'] },
+			],
+			paging: { page: 1, perPage: 5 },
+			query: term,
+		},
+	];
+}
 
+function getDataprepSearchParameters(term) {
+	return [http.get, `http://localhost:3000/api/search?path=/&name=${term}`];
+}
+
+function* search({ payload }) {
+	yield delay(500);
+	const [doc, tdp] = yield all([
+		yield call(...getDocumentationSearchParameters(payload)),
+		yield call(...getDataprepSearchParameters(payload)),
+	]);
+
+	console.log('[NC] aaa: ', doc.data);
+	console.log('[NC] bbb: ', tdp.data);
+}
+
+/*
+let task
+while (true) {
+  const { input } = yield take('INPUT_CHANGED')
+  if (task) {
+	yield cancel(task)
+  }
+  task = yield fork(handleInput, input)
+}
+*/
+
+function* searchFor() {
 	while (true) {
-		yield take('TOGGLE_SEARCH');
-		const state = yield select(state => state.cmf.components.getIn(path));
-		yield put(actions.components.mergeState(...path, { docked: !state.get('docked') }));
+		yield takeLatest(SEARCH_FOR, search);
+		console.log('[NC] take !');
 	}
 }
 
 export default {
-	toggle,
+	searchFor,
 };
