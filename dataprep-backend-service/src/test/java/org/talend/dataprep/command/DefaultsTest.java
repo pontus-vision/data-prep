22e0d700837a7c04b705735d581c5876ec469d74
Aurélien Leboulanger <35097656+aleboulanger@users.noreply.github.com>
2018-07-12 08:09:04 +0000

    chore: remove Defaults unit test about empty json response (#1465)
    The empty json response is not a use case handled by Defaults#convertResponse

--- dataprep-backend-service/src/test/java/org/talend/dataprep/command/DefaultsTest.java
+++ dataprep-backend-service/src/test/java/org/talend/dataprep/command/DefaultsTest.java
@@ -12,7 +12,9 @@
 
 package org.talend.dataprep.command;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.talend.dataprep.command.Defaults.toJson;
 
 import java.io.IOException;
@@ -163,20 +165,6 @@ public class DefaultsTest {
 
         // Then
         assertEquals("My Value", value.value);
-    }
-
-    @Test
-    public void shouldReadEmptyJsonResponse() throws Exception {
-        // Given
-        final ObjectMapper mapper = new ObjectMapper();
-        final BasicHttpResponse httpResponse = buildResponse();
-        httpResponse.setEntity(new StringEntity(""));
-
-        // When
-        final Response value = Defaults.convertResponse(mapper, Response.class).apply(buildRequest(), httpResponse);
-
-        // Then
-        assertNull(value);
     }
 
     @Test
