e1a72d28eec57e5d1ab15445b0257312e7245952
Henri-Mayeul de Benque <hmdebenque@talend.com>
2018-07-25 10:59:29 +0200

    Revert "Revert "fix(TDP-5895): fetch owner in author field" (#1499)" (#1505)
    This reverts commit d61eab5

--- dataprep-backend-service/src/main/java/org/talend/dataprep/conversions/inject/OwnerInjection.java
+++ dataprep-backend-service/src/main/java/org/talend/dataprep/conversions/inject/OwnerInjection.java
@@ -3,11 +3,12 @@ package org.talend.dataprep.conversions.inject;
 import java.util.function.BiFunction;
 
 import org.talend.dataprep.api.preparation.PreparationDTO;
+import org.talend.dataprep.preparation.store.PersistentPreparation;
 
 /**
  * An API to ease {@link org.talend.dataprep.api.share.Owner} injection in {@link PreparationDTO} instances.
  *
  * @see org.talend.dataprep.conversions.BeanConversionService#convert(Object, Class, BiFunction[])
  */
-public interface OwnerInjection extends BiFunction<Object, PreparationDTO, PreparationDTO> {
+public interface OwnerInjection extends BiFunction<PersistentPreparation, PreparationDTO, PreparationDTO> {
 }
