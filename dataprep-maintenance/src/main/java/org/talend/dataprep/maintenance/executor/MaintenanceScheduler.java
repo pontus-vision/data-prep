f404a79c6e6d5b3d5ad222669c1b08c31ec470b9
Fabien Abballe <fabballe@talend.com>
2018-07-27 17:05:56 +0200

    feat(TDP-5734/TDP-5964): Allow maintenance task to execute in a single tenant context at a time (#1507)
    * chore(TDP-5734): Allow per tenant grouped executions
    
    * Support code for maintenance tasks.
    * Add scheduled configuration to existing maintenance tasks.

---
+++ dataprep-maintenance/src/main/java/org/talend/dataprep/maintenance/executor/MaintenanceScheduler.java
@@ -0,0 +1,86 @@
+package org.talend.dataprep.maintenance.executor;
+
+import static org.apache.commons.lang.StringUtils.isNotEmpty;
+import static org.talend.dataprep.maintenance.executor.ScheduleFrequency.NIGHT;
+import static org.talend.dataprep.maintenance.executor.ScheduleFrequency.ONCE;
+import static org.talend.dataprep.maintenance.executor.ScheduleFrequency.REPEAT;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.talend.dataprep.security.Security;
+import org.talend.tenancy.ForAll;
+
+@Component
+public class MaintenanceScheduler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MaintenanceScheduler.class);
+
+    @Autowired
+    private List<MaintenanceTaskProcess> maintenanceTasks;
+
+    @Autowired
+    private ForAll forAll;
+
+    @Autowired
+    private Security security;
+
+    private Map<String, Long> runningTask = new ConcurrentHashMap<>();
+
+    @PostConstruct
+    public void launchOnceTask() {
+        runMaintenanceTask(ONCE);
+    }
+
+    @Scheduled(cron = "${maintenance.scheduled.cron}")
+    public void launchNightlyTask() {
+        runMaintenanceTask(NIGHT);
+    }
+
+    @Scheduled(fixedDelayString = "${maintenance.scheduled.fixed-delay}", initialDelayString = "${maintenance.scheduled.initial-delay}")
+    public void launchRepeatlyTask() {
+        runMaintenanceTask(REPEAT);
+    }
+
+    private void runMaintenanceTask(ScheduleFrequency frequency) {
+        forAll.execute(() -> true, () -> {
+            final String tenantId = security.getTenantId();
+            LOGGER.info("Starting scheduled task with frequency {} for tenant {}", frequency, tenantId);
+            maintenanceTasks.stream()
+                    .filter(task -> task.getFrequency() == frequency) //
+                    .forEach(task -> {
+                        String taskKey = task.getClass() + "_" + tenantId;
+                        if (isAlreadyRunning(taskKey)) {
+                            LOGGER.warn("Scheduled task {} for tenant {} is already running", task.getClass(), tenantId);
+                        } else {
+                            executeTask(tenantId, task, taskKey);
+                        }
+                    });
+            LOGGER.info("Scheduled task with frequency {} for tenant {} is finished", frequency, tenantId);
+        });
+    }
+
+    private void executeTask(String tenantId, MaintenanceTaskProcess task, String taskKey) {
+        Long startedTime = System.currentTimeMillis();
+        try {
+            LOGGER.debug("Scheduled task {} process for tenant {} started @ {}.", task.getClass(), tenantId, startedTime);
+            runningTask.put(taskKey, startedTime);
+            task.execute();
+            LOGGER.debug("Scheduled task {} process for tenant {} ended @ {}.", task.getClass(), tenantId, System.currentTimeMillis());
+        } finally {
+            runningTask.remove(taskKey);
+        }
+    }
+
+    protected boolean isAlreadyRunning(String taskKey) {
+        return isNotEmpty(taskKey) && runningTask.containsKey(taskKey);
+    }
+
+}
--- dataprep-maintenance/src/main/java/org/talend/dataprep/maintenance/upgrade/UpgradeTask.java
+++ dataprep-maintenance/src/main/java/org/talend/dataprep/maintenance/upgrade/UpgradeTask.java
@@ -4,13 +4,11 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 import java.util.function.Supplier;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Component;
+import org.talend.dataprep.maintenance.executor.MaintenanceTaskProcess;
+import org.talend.dataprep.maintenance.executor.ScheduleFrequency;
 import org.talend.dataprep.security.Security;
 import org.talend.dataprep.upgrade.UpgradeService;
 import org.talend.dataprep.upgrade.repository.UpgradeTaskRepository;
@@ -20,12 +18,16 @@ import org.talend.tenancy.ForAll;
  *
  */
 @Component
-public class UpgradeTask {
+public class UpgradeTask implements MaintenanceTaskProcess {
 
-    /** This class' logger. */
+    /**
+     * This class' logger.
+     */
     private static final Logger LOG = getLogger(UpgradeTask.class);
 
-    /** Service in charge of upgrading data from older versions. */
+    /**
+     * Service in charge of upgrading data from older versions.
+     */
     @Autowired
     private UpgradeService upgradeService;
 
@@ -33,27 +35,25 @@ public class UpgradeTask {
     private UpgradeTaskRepository repository;
 
     @Autowired
-    @Resource(name = "applicationEventMulticaster#executor")
-    private TaskExecutor executor;
-
-    @Autowired
     private ForAll forAll;
 
     @Autowired
     private Security security;
 
-    @PostConstruct
-    public void upgradeTask() {
-        LOG.info("Start method upgradeTask for all tenant");
-        executor.execute(() -> {
-            final Supplier<Boolean> needUpgradeCondition = () -> upgradeService.needUpgrade();
-            final Supplier<Boolean> hasRepositoryConfiguration = forAll.condition().operational(repository);
-            forAll.execute(() -> hasRepositoryConfiguration.get() && needUpgradeCondition.get(), () -> {
-                LOG.info("Performing upgrade for '{}'...", security.getTenantId());
-                upgradeService.upgradeVersion();
-                LOG.info("Performing upgrade done for '{}'.", security.getTenantId());
-            });
-        });
-        LOG.info("End method upgradeTask for all tenant");
+    public void performTask() {
+        LOG.info("Performing upgrade for '{}'...", security.getTenantId());
+        upgradeService.upgradeVersion();
+        LOG.info("Performing upgrade done for '{}'.", security.getTenantId());
+    }
+
+    public Supplier<Boolean> condition() {
+        final Supplier<Boolean> needUpgradeCondition = () -> upgradeService.needUpgrade();
+        final Supplier<Boolean> hasRepositoryConfiguration = forAll.condition().operational(repository);
+        return () -> needUpgradeCondition.get() && hasRepositoryConfiguration.get();
+    }
+
+    @Override
+    public ScheduleFrequency getFrequency() {
+        return ScheduleFrequency.ONCE;
     }
 }
