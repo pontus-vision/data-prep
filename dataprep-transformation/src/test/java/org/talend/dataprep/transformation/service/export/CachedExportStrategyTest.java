46a37b53fd40d701f49c8c50d4ea619a51bd6664
elogeart <elogeart@talend.com>
2018-08-09 11:44:07 +0200

    fix(TDP-5961): fix fetch more (#1532) (#1553)
    * fix(TDP-5961): fix fetch more
    * fix incorrect Integration Test for TDP-5733
    * base the fetch more on the correct sample, a filtered dataset generated by the FilterSamplingPipeline
    * apply the correct export strategy, that is PreparationSampleExportStrategy
    * fix(TDP-5961): flush OutputStream (otherwise leads to issues on Windows)
    * fix(TDP-5961): restore previous behavior when from == FILTER
    * re-enable use of CachedExportStrategy
    * enable asynchronism even if from == FILTER
    * clean code
    * fix(TDP-5961): add and fix unit tests
    * Add comment (FetchMore.feature, CacheExportStrategy.java)
    * Remove attribute declare in parent class (ApplyPreparationToContentStrategy.java)
    * Remove unnecessary call to exportParametersUtil.populateFromPreparationExportParameter (TransformationService.java)
    
    (cherry picked from commit a2a756f)

--- dataprep-transformation/src/test/java/org/talend/dataprep/transformation/service/export/CachedExportStrategyTest.java
+++ dataprep-transformation/src/test/java/org/talend/dataprep/transformation/service/export/CachedExportStrategyTest.java
@@ -14,6 +14,7 @@ package org.talend.dataprep.transformation.service.export;
 
 import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.talend.dataprep.api.export.ExportParameters.SourceType.FILTER;
 import static org.talend.dataprep.api.export.ExportParameters.SourceType.HEAD;
 
 import java.io.IOException;
@@ -25,6 +26,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.talend.ServiceBaseTest;
 import org.talend.dataprep.api.dataset.DataSet;
 import org.talend.dataprep.api.export.ExportParameters;
+import org.talend.dataprep.api.export.ExportParameters.SourceType;
 import org.talend.dataprep.api.preparation.Preparation;
 import org.talend.dataprep.cache.ContentCache;
 import org.talend.dataprep.preparation.store.PreparationRepository;
@@ -54,26 +56,40 @@ public class CachedExportStrategyTest extends ServiceBaseTest {
         preparationRepository.add(preparation);
 
         final TransformationCacheKey cacheKey = cacheKeyGenerator.generateContentKey("1234", "1234", "0", "text", HEAD, "");
+        putKeyInCache(cacheKey);
+        final TransformationCacheKey filteredSampleCacheKey = cacheKeyGenerator.generateContentKey("1234", "1234", "0", "text", FILTER, "");
+        putKeyInCache(filteredSampleCacheKey);
+    }
+
+    private void putKeyInCache(TransformationCacheKey cacheKey) {
         try (OutputStream text = cache.put(cacheKey, ContentCache.TimeToLive.DEFAULT)) {
             text.write("{}".getBytes());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    @Test
-    public void shouldAcceptIfCacheEntryExists() throws Exception {
+    private void doTestAcceptShouldPassIfCacheEntryExistsFrom(SourceType from) throws Exception {
         // Given
         final ExportParameters parameters = new ExportParameters();
         parameters.setDatasetId("1234");
         parameters.setPreparationId("1234");
         parameters.setStepId("0");
         parameters.setExportType("text");
-        parameters.setFrom(HEAD);
+        parameters.setFrom(from);
 
         // Then
         assertTrue(cachedExportStrategy.accept(parameters));
+    }
+
+    @Test
+    public void shouldAcceptIfCacheEntryExistsFromHEAD() throws Exception {
+        doTestAcceptShouldPassIfCacheEntryExistsFrom(HEAD);
+    }
+
+    @Test
+    public void shouldAcceptIfCacheEntryExistsFromFILTER() throws Exception {
+        doTestAcceptShouldPassIfCacheEntryExistsFrom(FILTER);
     }
 
     @Test
@@ -97,10 +113,10 @@ public class CachedExportStrategyTest extends ServiceBaseTest {
     }
 
     @Test
-    public void shouldNotAcceptNullParameterContent() throws Exception {
+    public void shouldNotAcceptWhenParameterContentIsPresent() throws Exception {
         // Then
         final ExportParameters parameters = new ExportParameters();
-        parameters.setContent(null);
+        parameters.setContent(new DataSet());
         assertFalse(cachedExportStrategy.accept(parameters));
     }
 
