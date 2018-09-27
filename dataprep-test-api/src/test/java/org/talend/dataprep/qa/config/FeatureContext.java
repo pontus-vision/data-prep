64fe3c15a1ba0fb855521102438a4657caf6f1f5
Stéphane Kermabon <skermabon@talend.com>
2018-09-27 15:38:46 +0200

    feat(TDP-4839): hdfs streams export ti (#1616)
    * Add HDFS dataset creation and streams export integration test

--- dataprep-test-api/src/test/java/org/talend/dataprep/qa/config/FeatureContext.java
+++ dataprep-test-api/src/test/java/org/talend/dataprep/qa/config/FeatureContext.java
@@ -99,6 +99,32 @@ public class FeatureContext {
     }
 
     /**
+     * Add suffix to a filename. This suffix is added before the filename extensions. For example "/tmp/file.csv" become
+     * "/tmp/file_654321.csv".
+     * @param fileName The filename.
+     *
+     * @return The suffixed filename.
+     */
+    public static String suffixFileName(String fileName) {
+        if (fileName.lastIndexOf('.') < 0
+                || (fileName.lastIndexOf('/') >= 0 && fileName.lastIndexOf('.') < fileName.lastIndexOf('/'))) {
+            return suffixName(fileName);
+        }
+        return fileName.substring(0, fileName.lastIndexOf('.')) + TI_SUFFIX_UID
+                + fileName.substring(fileName.lastIndexOf('.'));
+    }
+
+    /**
+     * Remove the suffix from the filename.
+     * @param fileName The suffixed filename.
+     *
+     * @return The filename without suffix.
+     */
+    public static String removeSuffixFileName(String fileName) {
+        return fileName.replaceAll(TI_SUFFIX_UID, "");
+    }
+
+    /**
      * Add a suffix to a name depending of the execution instance.
      *
      * @param folderPath to suffix.
