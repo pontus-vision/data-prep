57373e3337373268d4956a50b3fc1243536f9153
Fabien Abballe <fabballe@talend.com>
2018-11-22 11:10:04 +0100

    Revert "fix(TDP-6208): Enable async event on OnPrem bundle (#1757)" (#1765)
    This reverts commit 08386873a90e01d85fc89ded581e32411b125896.

--- dataprep-backend-service/src/main/java/org/talend/dataprep/configuration/DataPrepEvents.java
+++ dataprep-backend-service/src/main/java/org/talend/dataprep/configuration/DataPrepEvents.java
@@ -16,17 +16,14 @@ import javax.annotation.Resource;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ApplicationEventMulticaster;
-import org.springframework.context.event.SimpleApplicationEventMulticaster;
-import org.springframework.core.ResolvableType;
 import org.springframework.core.task.AsyncListenableTaskExecutor;
+import org.talend.dataprep.event.DataPrepEventsCaster;
 
 /**
- * For UT, all event are synchrone
+ * Setup the events management in data prep.
  */
 @SuppressWarnings("InsufficientBranchCoverage")
 @Configuration
@@ -44,19 +41,7 @@ public class DataPrepEvents {
     // do NOT change the name as it is important to replace the default application context event multi caster
     @Bean(name = "applicationEventMulticaster")
     public ApplicationEventMulticaster getDataPrepEventsCaster() {
-        return new TestApplicationEventMultiCaster();
-    }
-
-    private class TestApplicationEventMultiCaster extends SimpleApplicationEventMulticaster {
-
-        @Override
-        public void multicastEvent(final ApplicationEvent event, ResolvableType eventType) {
-            ResolvableType type = (eventType != null ? eventType : ResolvableType.forInstance(event));
-
-            for (final ApplicationListener<?> listener : getApplicationListeners(event, type)) {
-                invokeListener(listener, event);
-            }
-        }
+        return new DataPrepEventsCaster(executor, beanFactory);
     }
 
 }
