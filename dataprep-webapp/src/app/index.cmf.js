696ecf0b369cb90a70059ae3bed218e43c139b65
Fabien Rassinier <frassinier@talend.com>
2018-12-04 23:08:55 +0100

    Revert "chore(TDP-6356): remove redux-storage (#1769)"
    This reverts commit 564eab12100bd946c0a40e509588809c4a4a8864.

--- dataprep-webapp/src/app/index.cmf.js
+++ dataprep-webapp/src/app/index.cmf.js
@@ -7,17 +7,19 @@ import { App } from '@talend/react-cmf';
 import configure from './configure.cmf';
 
 function bootstrap() {
-	const { browserHistory, store } = configure();
-	/**
-	 * Render the CMF App
-	 */
-	render(
-		<App
-			store={store}
-			history={syncHistoryWithStore(browserHistory, store)}
-		/>,
-		document.getElementById('app'),
-	);
+	return configure()
+		.then(({ browserHistory, store }) => {
+			/**
+			 * Render the CMF App
+			 */
+			render(
+				<App
+					store={store}
+					history={syncHistoryWithStore(browserHistory, store)}
+				/>,
+				document.getElementById('app'),
+			);
+		});
 }
 
 export default bootstrap;
