ee04e5ed4f3345de1047d8fb8711c41cecf41402
Pontus <lmartins@pontusnetworks.com>
2019-05-28 10:44:02 +0100

    removed yarn.lock file; also commented out talend refs on sass.conf

--- dataprep-webapp/config/sass.conf.js
+++ dataprep-webapp/config/sass.conf.js
@@ -2,6 +2,6 @@ module.exports = `
 	$brand-primary: #00A1B3;
 	$brand-secondary:  #168AA6;
 
-	@import '~@talend/bootstrap-theme/src/theme/guidelines';
-	@import "~@talend/bootstrap-theme/src/theme/variations/tdp";
+#	@import '~@talend/bootstrap-theme/src/theme/guidelines';
+#	@import "~@talend/bootstrap-theme/src/theme/variations/tdp";
 `;
